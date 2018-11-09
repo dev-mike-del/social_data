fe9206a81d2af2aff83aa545fa87fb3a1c21312c
dev-mike-del <delgado.portland@gmail.com>
2018-11-09 00:08:40 -0800

    Update twitter_search/views to use sessions to pass the twitter screen_name to the get_twitter_profile view

--- social_data/twitter_search/views.py
+++ social_data/twitter_search/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .forms import Screen_Name_Form
 
@@ -11,9 +12,9 @@ def get_screen_name(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
+            request.session['screen_name'] = form.cleaned_data['screen_name']
             # redirect to a new URL:
-            return HttpResponseRedirect('../twitter_search/twitter_profile_data')
+            return HttpResponseRedirect(reverse('twitter_search:twitter_profile_data'))
     # if a GET (or any other method) we'll create a blank form
     else:
         form = Screen_Name_Form()
@@ -23,5 +24,8 @@ def get_screen_name(request):
                   {'form': form})
 
 def get_twitter_profile(request):
+    screen_name = request.session.get('screen_name')
+    print(f"GET_TWITTER_PROFILE view for {screen_name}")
     return render(request,
                   'twitter_search/twitter_profile.html')
+
